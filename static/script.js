fafb5fb1a2e55967186674e1d83f822ddc97fb4c
Ahmedsaed <ahmedsaed2652003@gmail.com>
2022-01-29 20:55:26 +0200

    update

---
+++ static/script.js
@@ -0,0 +1,8 @@
+var questions_body = document.getElementById("questions_body");
+var welcome_message = document.getElementById("welcome_message");
+
+function displaySurvey()
+{
+    questions_body.hidden = false;
+    welcome_message.hidden = true;
+}
